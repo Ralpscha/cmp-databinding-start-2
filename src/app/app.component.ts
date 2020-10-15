826ed5c255b80d3a821af192b2a376943dc68d32
Ralph van Schaik <ralph.van.schaik@me.com>
2020-10-15 16:34:25 +0200

    68 Eventemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,20 @@ export class AppComponent {
     {type:'blueprint',name:'Rudolf Hess Tekening', content: 'Berlijn stratenplan'}
   ];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 
 }
